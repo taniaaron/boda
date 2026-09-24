317e2f03464e9a5a6a4b0eb63d5c428d1671f883
taniaaron <aaron.vlc15@gmail.com>
2026-09-24 10:44:48 +0200

    Añadir interacciones de la web

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelector('.menu').addEventListener('click',()=>document.querySelector('.links').classList.toggle('open'));document.querySelectorAll('.links a').forEach(a=>a.addEventListener('click',()=>document.querySelector('.links').classList.remove('open')));
